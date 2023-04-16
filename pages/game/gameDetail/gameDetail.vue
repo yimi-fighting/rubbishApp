e1a8d51a8196ce5e5a5fe3bdd6df5a977c092c77
李妙红 <1348986909@qq.com>
2023-04-16 22:36:30 +0800

    初始化卡片，以及游戏主界面

--- pages/game/gameDetail/gameDetail.vue
+++ pages/game/gameDetail/gameDetail.vue
@@ -1,250 +1,457 @@
 <template>
   <view class="container">
     <div class="card_container">
-      <div class="card">
-        <div class="content">
-          {{aaa}}
-        </div>
-      </div><div class="card cover">
-        <div class="content">
-          {{aaa}}
-        </div>
-      </div>
+      <div class="card" v-for="item in cardList" :key="item.key" :style="item.style">{{item.content.name}}</div>
     </div>
     <div class="tool">
-      <button>取出三张卡片</button>
-      <button>随机</button>
-      <button>再来一轮</button>
+      <button @click="removeThree">取出三张卡片</button>
+      <button @click="random">随机</button>
+      <button @click="again">再来一轮</button>
     </div>
     <div class="three_card_container">
-      <div class="card">
-        <div class="content">
-          {{aaa}}
-        </div>
-      </div> <div class="card">
-        <div class="content">
-          {{aaa}}
-        </div>
-      </div> <div class="card">
-        <div class="content">
-          {{aaa}}
-        </div>
-      </div></div>
-    <div class="choose_card_container">
-      <div class="card">
-        <div class="content">
-          {{aaa}}
-        </div>
-      </div>   <div class="card">
-        <div class="content">
-          {{aaa}}
-        </div>
-      </div>   <div class="card">
-        <div class="content">
-          {{aaa}}
-        </div>
-      </div>   <div class="card">
-        <div class="content">
-          {{aaa}}
-        </div>
-      </div>   <div class="card">
-        <div class="content">
-          {{aaa}}
-        </div>
-      </div>   <div class="card">
-        <div class="content">
-          {{aaa}}
-        </div>
-      </div>   <div class="card">
-        <div class="content">
-          {{aaa}}
-        </div>
-      </div>
+      <div class="card">{{aaa}}</div>
+      <div class="card">{{aaa}}</div>
+      <div class="card">{{aaa}}</div>
+      <div class="card top">{{aaa}}</div>
     </div>
-  
+    <div class="choose_card_container">
+      <div class="card">{{aaa}}</div>
+      <div class="card">{{aaa}}</div>
+      <div class="card">{{aaa}}</div>
+      <div class="card top">{{aaa}}</div>
+    </div>
+
   </view>
 </template>
 
 <script>
-  class Card{
-    // 0:可回收垃圾；1：有害垃圾；2：湿垃圾；3：干垃圾
-    static contentType=[
-      {
-        name:'📦',
-        class:'0',
-        style:'background: #73b0ff'
-      }, {
-        name:'📚',
-        class:'0',
-        style:'background: #73b0ff'
-      }, {
-        name:'🔩',
-        class:'0',
-        style:'background: #73b0ff'
-      }, {
-        name:'🍶',
-        class:'0',
-        style:'background: #73b0ff'
-      }, {
-        name:'👗',
-        class:'0',
-        style:'background: #73b0ff'
-      },{
-        name:'💊',
-        class:'1',
-        style:'background: #ff5c74'
-      },{
-        name:'🔋',
-        class:'1',
-        style:'background: #ff5c74'
-      },{
-        name:'🧪',
-        class:'1',
-        style:'background: #ff5c74'
-      },{
-        name:'💉',
-        class:'1',
-        style:'background: #ff5c74'
-      },{
-        name:'🎨',
-        class:'1',
-        style:'background: #ff5c74'
-      },{
-        name:'🚨',
-        class:'1',
-        style:'background: #ff5c74'
-      },{
-        name:'🍎',
-        class:'2',
-        style:'background: #82eb62'
-      },{
-        name:'🍗',
-        class:'2',
-        style:'background: #82eb62'
-      },{
-        name:'🍌',
-        class:'2',
-        style:'background: #82eb62'
-      },{
-        name:'🌿',
-        class:'2',
-        style:'background: #82eb62'
-      },{
-        name:'🍂',
-        class:'2',
-        style:'background: #82eb62'
-      },{
-        name:'🐟',
-        class:'2',
-        style:'background: #82eb62'
-      },{
-        name:'🧻',
-        class:'3',
-        style:'background: #ced5b2'
-      },{
-        name:'🚬',
-        class:'3',
-        style:'background: #ced5b2'
-      },{
-        name:'👞',
-        class:'3',
-        style:'background: #ced5b2'
-      },{
-        name:'🧯',
-        class:'3',
-        style:'background: #ced5b2'
-      }
-    ]
+  
+  class Card {
+    // 卡片的宽高,只有原本高度的一半，因为之后一个卡片占2*2的数组大小，方便形成上层卡片压到下层卡片的某个角的效果。
+    static x = 20;
+    static y = 25;
+    static contentType=[];
+    constructor({
+      x,
+      y,
+      z,
+      key
+    }) {
+      this.x = x;
+      this.y = y;
+      this.z = z;
+      this.key = key;
+      this.val = key;
+      this.content = {};
+      this.style = `top: ${y*Card.y+30}px;left:${x*Card.x+30}px;`
+    }
+    setVal(val) {
+      this.val = val-1;
+      this.content = Card.contentType[val-1]
+      this.style+=`${this.content.style}`
+    }
+    static setContentType(list){
+      Card.contentType=list
+    }
   }
   export default {
     data() {
       return {
-        options:{},
-        aaa:'🧻'
+        options: {},
+        aaa: '🧻',
+        // 所有卡片
+        cardList: [],
+        // 保存的三张卡片
+        saveList: [],
+        // 选择的卡片
+        penddingList: [],
+        // 判断道具是否已使用过
+        tools: {
+          three: true,
+          random: true,
+        },
+        // 判断是否以及获胜
+        winner: false,
+        // 数组的很轴和纵轴
+        xUnit: 0,
+        yUnit: 0,
       };
     },
-    onLoad(option) {
-      // this.options=JSON.parse(option.options)
-      this.options={
-        x: 6,
-        y: 5,
-        z: 3,
-        random:0.3,
-        maxCard:10,
+    methods: {
+      // 初始化游戏
+      init(options) {
+        // 清空list
+        this.cardList = []
+        this.penddingList = []
+        this.saveList = []
+        // 重置tools
+        this.tools.three = true
+        this.tools.random = true
+        // 绘制卡片地图
+        this.getMaps(options)
+        // 给卡片设置图案
+        // 计算遮挡关系
+      },
+      // 绘制卡片地图
+      getMaps(options) {
+        // 初始化网络地图
+        let cardMap = this.initMap(options)
+        // 根据卡片密度，向cardmap中放入card,此时的卡片没有内容
+        cardMap = this.setCard(cardMap, options)
+        // 设置卡片的内容
+        this.setContent(options)
+      },
+      // 设置卡片的内容
+      setContent(options) {
+        const { maxCard} = options
+        const valStack = new Array(maxCard)
+        // 给卡片设置值
+        this.cardList.forEach((item) => {
+          // 随机一个种类
+          const value = Math.ceil(Math.random() * maxCard);
+          if (valStack[value]) {
+            // 将item赋值这个种类的卡片
+            valStack[value].push(item);
+            // 将他们进行三个三个配对
+            if (valStack[value].length === 3) {
+              valStack[value].forEach((item) => {
+                item.setVal(value);
+              });
+              valStack[value] = null;
+            }
+          } else {
+            valStack[value] = [item];
+          }
+        });
+        // 对为进行分类的重新分类
+        let count = 2;
+        valStack.forEach(list => {
+          list && list.map(item => {
+            count++
+            item.setVal(Math.floor(count / 3))
+          })
+        })
+      },
+      // 初始化地图，确认卡片位置
+      setCard(map, options) {
+        const {
+          x,
+          y,
+          z,
+          random
+        } = options
+        let key = 0
+        const cardList = []
+        const shrinkSpeed = 3
+        for (let k = 0; k < z; k++) {
+          const shrink = Math.floor((z - k - 1) / shrinkSpeed)
+          const shrinkX = Math.min(Math.floor(this.xUnit / 2) - 2, shrink)
+          const shrinkY = Math.min(Math.floor(this.yUnit / 2) - 2, shrink)
+          // 行
+          // -1是因为y是*2后的，最后一行不能放置卡片，不然就会超出边界
+          for (let i = shrinkY; i < this.yUnit - 1 - shrinkY; i++) {
+            // 列
+            for (let j = shrinkX; j < Math.ceil((this.xUnit - 1) / 2); j++) {
+              let canSetCard = true
+              if (j > 0 && map[k][i][j - 1]) {
+                // 左边不能有卡片
+                canSetCard = false
+              } else if (i > 0 && map[k][i - 1][j]) {
+                // 上面不能有卡片
+                canSetCard = false
+              } else if (i > 0 && j > 0 && map[k][i - 1][j - 1]) {
+                // 左上角不能有卡片
+                canSetCard = false
+              } else if (i > 0 && map[k][i - 1][j + 1]) {
+                // 右上角不能有卡片
+                canSetCard = false
+              } else if (k > 0 && map[k - 1][i][j]) {
+                // 正底下不能有卡片
+                canSetCard = false
+              }
+              // 如果这个位置可以放置卡片，那么根据随机数，设置卡片的密度
+              if (canSetCard) {
+                const rand = Math.random()
+                if (rand < random) {
+                  // 设置卡片
+                  const card = new Card({
+                    x: j,
+                    y: i,
+                    z: k,
+                    key
+                  })
+                  key++
+                  map[k][i][j] = card
+                  cardList.push(card)
+                  // 对称放置卡片
+                  const mirror = this.xUnit - 2 - j
+                  if (mirror > j) {
+                    const item = new Card({
+                      x: mirror,
+                      y: i,
+                      z: k,
+                      key
+                    })
+                    map[k][i][mirror] = item
+                    key++
+                    cardList.push(item)
+                  }
+                }
+              }
+            }
+          }
+        }
+        // 卡片形成三的倍数,去掉顶层的卡片，因为顶层的卡片最多
+        cardList.reverse()
+        for (let i = 0; i < cardList.length % 3; i++) {
+          let item = cardList.pop()
+          map[item.z][item.y][item.x] = 0
+        }
+        cardList.reverse()
+        this.cardList = cardList
+        return map
+      },
+      // 初始化网络地图
+      initMap(options) {
+        const {
+          x,
+          y,
+          z
+        } = options
+        this.xUnit = x * 2
+        this.yUnit = y * 2
+        const cardMap = new Array(z)
+        for (var i = 0; i < z; i++) {
+          cardMap[i] = new Array(this.yUnit)
+          for (var j = 0; j < this.yUnit; j++) {
+            cardMap[i][j] = new Array(this.xUnit).fill(0)
+          }
+        }
+        return cardMap
+      },
+      // 再来一轮
+      again() {
+        this.init()
+      },
+      // 随机
+      random() {
+
+      },
+      // 取出三张卡片
+      removeThree() {
+
       }
-      console.log(this.options);
-    }
+    },
+    onLoad(option) {
+          // 0:可回收垃圾；1：有害垃圾；2：湿垃圾；3：干垃圾
+      const contentList= [{
+          name: '📦',
+          class: '0',
+          style: 'background: #73b0ff'
+        }, {
+          name: '📚',
+          class: '0',
+          style: 'background: #73b0ff'
+        }, {
+          name: '🔩',
+          class: '0',
+          style: 'background: #73b0ff'
+        }, {
+          name: '🍶',
+          class: '0',
+          style: 'background: #73b0ff'
+        }, {
+          name: '👗',
+          class: '0',
+          style: 'background: #73b0ff'
+        }, {
+          name: '💊',
+          class: '1',
+          style: 'background: #ff5c74'
+        }, {
+          name: '🔋',
+          class: '1',
+          style: 'background: #ff5c74'
+        }, {
+          name: '🧪',
+          class: '1',
+          style: 'background: #ff5c74'
+        }, {
+          name: '💉',
+          class: '1',
+          style: 'background: #ff5c74'
+        }, {
+          name: '🎨',
+          class: '1',
+          style: 'background: #ff5c74'
+        }, {
+          name: '🚨',
+          class: '1',
+          style: 'background: #ff5c74'
+        }, {
+          name: '🍎',
+          class: '2',
+          style: 'background: #82eb62'
+        }, {
+          name: '🍗',
+          class: '2',
+          style: 'background: #82eb62'
+        }, {
+          name: '🍌',
+          class: '2',
+          style: 'background: #82eb62'
+        }, {
+          name: '🌿',
+          class: '2',
+          style: 'background: #82eb62'
+        }, {
+          name: '🍂',
+          class: '2',
+          style: 'background: #82eb62'
+        }, {
+          name: '🐟',
+          class: '2',
+          style: 'background: #82eb62'
+        }, {
+          name: '🧻',
+          class: '3',
+          style: 'background: #ced5b2'
+        }, {
+          name: '🚬',
+          class: '3',
+          style: 'background: #ced5b2'
+        }, {
+          name: '👞',
+          class: '3',
+          style: 'background: #ced5b2'
+        }, {
+          name: '🧯',
+          class: '3',
+          style: 'background: #ced5b2'
+        }]
+      this.options=JSON.parse(option.options)
+      // 随机卡片样式数组
+      // 洗牌算法
+      let shuffle=(arr)=>{
+        for (let i = arr.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+        return arr;
+      }
+      const selected=shuffle(contentList).slice(0,this.options.maxCard)
+       Card.setContentType(selected)
+        this.init(this.options)
+    },
+    
   }
 </script>
 
 <style lang="scss">
-  .container{
+  .container {
     width: 100vw;
     height: 100vh;
     position: relative;
-    .card_container{
-        padding: 30px;
-        height: 300px;
-        background-color: pink;
-      }
 
-    .tool{
+    .card_container {
+      position: relative;
+      padding: 30px;
+      height: 300px;
+    }
+
+    .tool {
       // position: absolute;
       display: flex;
       width: 100vw;
       // transform: translate(-50%,0);
       justify-content: space-around;
     }
-    .three_card_container{
+
+    .three_card_container {
+      position: relative;
       display: flex;
       width: 100%;
       height: 55px;
-      padding:10px 20px;
+      padding: 10px 20px;
       margin-top: 10px;
+
       // background-color: green;
-      .card{
+      .card {
         margin-right: 7px;
       }
     }
-    .choose_card_container{
+
+    .choose_card_container {
+      position: relative;
       display: flex;
       margin-top: 20px;
       width: 95%;
       height: 78px;
-      padding:10px 15px;
+      padding: 10px 15px;
       background-color: #82eb62;
       border: 1px solid black;
       border-radius: 20px;
       box-sizing: border-box;
       margin: 10px 10px;
-      .card{
+
+      .card {
         margin-right: 7px;
       }
     }
   }
- .card{
-   width: 40px;
-   height: 50px;
-   // background-color: pink;
-   border: 1px solid black;
-   border-radius: 5px;
-   
-   .content{
-     padding: 2px;
-     height: 40px;
-     // background-color: #73d3ff;
-     background-color: white;
-     font-size: 25px;
-     text-align: center;
-     line-height: 40px;
-     border-radius: 5px;
-     // pointer-events: none;
-       box-shadow: 0px 3px 0 0 #999, 0 8px 0 0 #666, 0 8px 0 2px #000, 0 0 0 2px #000;
-       // box-shadow: 0px 3px 0 0 #fff, 0 8px 0 0 #ddd, 0 8px 0 2px #333, 0 0 0 2px #333;
-   }
- }
- .cover{
-   background-color: #666;
- }
 
+  .card {
+    font-size: 28px;
+    text-align: center;
+    position: absolute;
+    border-radius: 2px;
+    box-sizing: border-box;
+    background: #ddd;
+    opacity: 1;
+    cursor: pointer;
+    transition: all 0.3s;
+    box-shadow: 0px 3px 0 0 #fff, 0 8px 0 0 #ddd, 0 8px 0 2px #333, 0 0 0 2px #333;
+
+    &:hover {
+      transform: scale3d(1.1, 1.1, 1.1);
+      z-index: 1;
+    }
+
+    // width: 40px;
+    // height: 50px;
+    // // background-color: pink;
+    // border: 1px solid black;
+    // border-radius: 5px;
+
+    // .content{
+    //   padding: 2px;
+    //   height: 40px;
+    //   // background-color: #73d3ff;
+    //   background-color: white;
+    //   font-size: 25px;
+    //   text-align: center;
+    //   line-height: 40px;
+    //   border-radius: 5px;
+    //   // pointer-events: none;
+    //     // box-shadow: 0px 3px 0 0 #999, 0 8px 0 0 #666, 0 8px 0 2px #000, 0 0 0 2px #000;
+    //     box-shadow: 0px 3px 0 0 #fff, 0 8px 0 0 #ddd, 0 8px 0 2px #333, 0 0 0 2px #333;
+    // }
+  }
+
+  .cover {
+    pointer-events: none;
+    box-shadow: 0px 3px 0 0 #999, 0 8px 0 0 #666, 0 8px 0 2px #000, 0 0 0 2px #000;
+  }
+
+  .cover:after {
+    border-radius: 2px;
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    background: #000;
+    opacity: 0.55;
+  }
 </style>
--- unpackage/dist/dev/mp-weixin/pages/game/gameDetail/gameDetail.js
+++ unpackage/dist/dev/mp-weixin/pages/game/gameDetail/gameDetail.js
@@ -121,8 +121,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
-var _createClass2 = _interopRequireDefault(__webpack_require__(/*! @babel/runtime/helpers/createClass */ 24));
 var _classCallCheck2 = _interopRequireDefault(__webpack_require__(/*! @babel/runtime/helpers/classCallCheck */ 23));
+var _createClass2 = _interopRequireDefault(__webpack_require__(/*! @babel/runtime/helpers/createClass */ 24));
 var _defineProperty2 = _interopRequireDefault(__webpack_require__(/*! @babel/runtime/helpers/defineProperty */ 11));
 //
 //
@@ -150,152 +150,323 @@ var _defineProperty2 = _interopRequireDefault(__webpack_require__(/*! @babel/run
 //
 //
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-var Card = /*#__PURE__*/(0, _createClass2.default)(function Card() {
-  (0, _classCallCheck2.default)(this, Card);
-});
-(0, _defineProperty2.default)(Card, "contentType", [{
-  name: '📦',
-  class: '0',
-  style: 'background: #73b0ff'
-}, {
-  name: '📚',
-  class: '0',
-  style: 'background: #73b0ff'
-}, {
-  name: '🔩',
-  class: '0',
-  style: 'background: #73b0ff'
-}, {
-  name: '🍶',
-  class: '0',
-  style: 'background: #73b0ff'
-}, {
-  name: '👗',
-  class: '0',
-  style: 'background: #73b0ff'
-}, {
-  name: '💊',
-  class: '1',
-  style: 'background: #ff5c74'
-}, {
-  name: '🔋',
-  class: '1',
-  style: 'background: #ff5c74'
-}, {
-  name: '🧪',
-  class: '1',
-  style: 'background: #ff5c74'
-}, {
-  name: '💉',
-  class: '1',
-  style: 'background: #ff5c74'
-}, {
-  name: '🎨',
-  class: '1',
-  style: 'background: #ff5c74'
-}, {
-  name: '🚨',
-  class: '1',
-  style: 'background: #ff5c74'
-}, {
-  name: '🍎',
-  class: '2',
-  style: 'background: #82eb62'
-}, {
-  name: '🍗',
-  class: '2',
-  style: 'background: #82eb62'
-}, {
-  name: '🍌',
-  class: '2',
-  style: 'background: #82eb62'
-}, {
-  name: '🌿',
-  class: '2',
-  style: 'background: #82eb62'
-}, {
-  name: '🍂',
-  class: '2',
-  style: 'background: #82eb62'
-}, {
-  name: '🐟',
-  class: '2',
-  style: 'background: #82eb62'
-}, {
-  name: '🧻',
-  class: '3',
-  style: 'background: #ced5b2'
-}, {
-  name: '🚬',
-  class: '3',
-  style: 'background: #ced5b2'
-}, {
-  name: '👞',
-  class: '3',
-  style: 'background: #ced5b2'
-}, {
-  name: '🧯',
-  class: '3',
-  style: 'background: #ced5b2'
-}]);
+var Card = /*#__PURE__*/function () {
+  // 卡片的宽高,只有原本高度的一半，因为之后一个卡片占2*2的数组大小，方便形成上层卡片压到下层卡片的某个角的效果。
+
+  function Card(_ref) {
+    var x = _ref.x,
+      y = _ref.y,
+      z = _ref.z,
+      key = _ref.key;
+    (0, _classCallCheck2.default)(this, Card);
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.key = key;
+    this.val = key;
+    this.content = {};
+    this.style = "top: ".concat(y * Card.y + 30, "px;left:").concat(x * Card.x + 30, "px;");
+  }
+  (0, _createClass2.default)(Card, [{
+    key: "setVal",
+    value: function setVal(val) {
+      this.val = val - 1;
+      this.content = Card.contentType[val - 1];
+      this.style += "".concat(this.content.style);
+    }
+  }], [{
+    key: "setContentType",
+    value: function setContentType(list) {
+      Card.contentType = list;
+    }
+  }]);
+  return Card;
+}();
+(0, _defineProperty2.default)(Card, "x", 20);
+(0, _defineProperty2.default)(Card, "y", 25);
+(0, _defineProperty2.default)(Card, "contentType", []);
 var _default = {
   data: function data() {
     return {
       options: {},
-      aaa: '🧻'
+      aaa: '🧻',
+      // 所有卡片
+      cardList: [],
+      // 保存的三张卡片
+      saveList: [],
+      // 选择的卡片
+      penddingList: [],
+      // 判断道具是否已使用过
+      tools: {
+        three: true,
+        random: true
+      },
+      // 判断是否以及获胜
+      winner: false,
+      // 数组的很轴和纵轴
+      xUnit: 0,
+      yUnit: 0
     };
   },
+  methods: {
+    // 初始化游戏
+    init: function init(options) {
+      // 清空list
+      this.cardList = [];
+      this.penddingList = [];
+      this.saveList = [];
+      // 重置tools
+      this.tools.three = true;
+      this.tools.random = true;
+      // 绘制卡片地图
+      this.getMaps(options);
+      // 给卡片设置图案
+      // 计算遮挡关系
+    },
+    // 绘制卡片地图
+    getMaps: function getMaps(options) {
+      // 初始化网络地图
+      var cardMap = this.initMap(options);
+      // 根据卡片密度，向cardmap中放入card,此时的卡片没有内容
+      cardMap = this.setCard(cardMap, options);
+      // 设置卡片的内容
+      this.setContent(options);
+    },
+    // 设置卡片的内容
+    setContent: function setContent(options) {
+      var maxCard = options.maxCard;
+      var valStack = new Array(maxCard);
+      // 给卡片设置值
+      this.cardList.forEach(function (item) {
+        // 随机一个种类
+        var value = Math.ceil(Math.random() * maxCard);
+        if (valStack[value]) {
+          // 将item赋值这个种类的卡片
+          valStack[value].push(item);
+          // 将他们进行三个三个配对
+          if (valStack[value].length === 3) {
+            valStack[value].forEach(function (item) {
+              item.setVal(value);
+            });
+            valStack[value] = null;
+          }
+        } else {
+          valStack[value] = [item];
+        }
+      });
+      // 对为进行分类的重新分类
+      var count = 2;
+      valStack.forEach(function (list) {
+        list && list.map(function (item) {
+          count++;
+          item.setVal(Math.floor(count / 3));
+        });
+      });
+    },
+    // 初始化地图，确认卡片位置
+    setCard: function setCard(map, options) {
+      var x = options.x,
+        y = options.y,
+        z = options.z,
+        random = options.random;
+      var key = 0;
+      var cardList = [];
+      var shrinkSpeed = 3;
+      for (var k = 0; k < z; k++) {
+        var shrink = Math.floor((z - k - 1) / shrinkSpeed);
+        var shrinkX = Math.min(Math.floor(this.xUnit / 2) - 2, shrink);
+        var shrinkY = Math.min(Math.floor(this.yUnit / 2) - 2, shrink);
+        // 行
+        // -1是因为y是*2后的，最后一行不能放置卡片，不然就会超出边界
+        for (var i = shrinkY; i < this.yUnit - 1 - shrinkY; i++) {
+          // 列
+          for (var j = shrinkX; j < Math.ceil((this.xUnit - 1) / 2); j++) {
+            var canSetCard = true;
+            if (j > 0 && map[k][i][j - 1]) {
+              // 左边不能有卡片
+              canSetCard = false;
+            } else if (i > 0 && map[k][i - 1][j]) {
+              // 上面不能有卡片
+              canSetCard = false;
+            } else if (i > 0 && j > 0 && map[k][i - 1][j - 1]) {
+              // 左上角不能有卡片
+              canSetCard = false;
+            } else if (i > 0 && map[k][i - 1][j + 1]) {
+              // 右上角不能有卡片
+              canSetCard = false;
+            } else if (k > 0 && map[k - 1][i][j]) {
+              // 正底下不能有卡片
+              canSetCard = false;
+            }
+            // 如果这个位置可以放置卡片，那么根据随机数，设置卡片的密度
+            if (canSetCard) {
+              var rand = Math.random();
+              if (rand < random) {
+                // 设置卡片
+                var card = new Card({
+                  x: j,
+                  y: i,
+                  z: k,
+                  key: key
+                });
+                key++;
+                map[k][i][j] = card;
+                cardList.push(card);
+                // 对称放置卡片
+                var mirror = this.xUnit - 2 - j;
+                if (mirror > j) {
+                  var item = new Card({
+                    x: mirror,
+                    y: i,
+                    z: k,
+                    key: key
+                  });
+                  map[k][i][mirror] = item;
+                  key++;
+                  cardList.push(item);
+                }
+              }
+            }
+          }
+        }
+      }
+      // 卡片形成三的倍数,去掉顶层的卡片，因为顶层的卡片最多
+      cardList.reverse();
+      for (var _i = 0; _i < cardList.length % 3; _i++) {
+        var _item = cardList.pop();
+        map[_item.z][_item.y][_item.x] = 0;
+      }
+      cardList.reverse();
+      this.cardList = cardList;
+      return map;
+    },
+    // 初始化网络地图
+    initMap: function initMap(options) {
+      var x = options.x,
+        y = options.y,
+        z = options.z;
+      this.xUnit = x * 2;
+      this.yUnit = y * 2;
+      var cardMap = new Array(z);
+      for (var i = 0; i < z; i++) {
+        cardMap[i] = new Array(this.yUnit);
+        for (var j = 0; j < this.yUnit; j++) {
+          cardMap[i][j] = new Array(this.xUnit).fill(0);
+        }
+      }
+      return cardMap;
+    },
+    // 再来一轮
+    again: function again() {
+      this.init();
+    },
+    // 随机
+    random: function random() {},
+    // 取出三张卡片
+    removeThree: function removeThree() {}
+  },
   onLoad: function onLoad(option) {
-    // this.options=JSON.parse(option.options)
-    this.options = {
-      x: 6,
-      y: 5,
-      z: 3,
-      random: 0.3,
-      maxCard: 10
+    // 0:可回收垃圾；1：有害垃圾；2：湿垃圾；3：干垃圾
+    var contentList = [{
+      name: '📦',
+      class: '0',
+      style: 'background: #73b0ff'
+    }, {
+      name: '📚',
+      class: '0',
+      style: 'background: #73b0ff'
+    }, {
+      name: '🔩',
+      class: '0',
+      style: 'background: #73b0ff'
+    }, {
+      name: '🍶',
+      class: '0',
+      style: 'background: #73b0ff'
+    }, {
+      name: '👗',
+      class: '0',
+      style: 'background: #73b0ff'
+    }, {
+      name: '💊',
+      class: '1',
+      style: 'background: #ff5c74'
+    }, {
+      name: '🔋',
+      class: '1',
+      style: 'background: #ff5c74'
+    }, {
+      name: '🧪',
+      class: '1',
+      style: 'background: #ff5c74'
+    }, {
+      name: '💉',
+      class: '1',
+      style: 'background: #ff5c74'
+    }, {
+      name: '🎨',
+      class: '1',
+      style: 'background: #ff5c74'
+    }, {
+      name: '🚨',
+      class: '1',
+      style: 'background: #ff5c74'
+    }, {
+      name: '🍎',
+      class: '2',
+      style: 'background: #82eb62'
+    }, {
+      name: '🍗',
+      class: '2',
+      style: 'background: #82eb62'
+    }, {
+      name: '🍌',
+      class: '2',
+      style: 'background: #82eb62'
+    }, {
+      name: '🌿',
+      class: '2',
+      style: 'background: #82eb62'
+    }, {
+      name: '🍂',
+      class: '2',
+      style: 'background: #82eb62'
+    }, {
+      name: '🐟',
+      class: '2',
+      style: 'background: #82eb62'
+    }, {
+      name: '🧻',
+      class: '3',
+      style: 'background: #ced5b2'
+    }, {
+      name: '🚬',
+      class: '3',
+      style: 'background: #ced5b2'
+    }, {
+      name: '👞',
+      class: '3',
+      style: 'background: #ced5b2'
+    }, {
+      name: '🧯',
+      class: '3',
+      style: 'background: #ced5b2'
+    }];
+    this.options = JSON.parse(option.options);
+    // 随机卡片样式数组
+    // 洗牌算法
+    var shuffle = function shuffle(arr) {
+      for (var i = arr.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var _ref2 = [arr[j], arr[i]];
+        arr[i] = _ref2[0];
+        arr[j] = _ref2[1];
+      }
+      return arr;
     };
-    console.log(this.options);
+    var selected = shuffle(contentList).slice(0, this.options.maxCard);
+    Card.setContentType(selected);
+    this.init(this.options);
   }
 };
 exports.default = _default;
